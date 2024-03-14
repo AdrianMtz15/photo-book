b6acc125063867ddd7a8e938fb31369b041724c1
corps01 <ocorpus1@outlook.com>
2024-03-14 17:48:38 -0600

    tabs

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,23 +1,49 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../context/UserContext";
 import PhotoGrid from "./PhotoGrid";
 import Landing from "./Landing";
 import MobileModal from "../components/MobileModal";
 import InstagramWidget from "./InstagramWidget";
+import { IonLabel, IonSegment, IonSegmentButton } from "@ionic/react";
 
 const Main = () => {
   const { userLoggedIn } = useContext(UserContext);
+
+  const [tab, setTab] = useState("photoGrid");
 
   useEffect(() => {
     userLoggedIn();
   }, []);
 
+  const renderTabs = () => {
+    return (
+      <IonSegment value={tab}>
+        <IonSegmentButton value="photoGrid" onClick={() => setTab("photoGrid")}>
+          <IonLabel>Galería de Fotos</IonLabel>
+        </IonSegmentButton>
+        <IonSegmentButton value="instagram" onClick={() => setTab("instagram")}>
+          <IonLabel>Instagram</IonLabel>
+        </IonSegmentButton>
+      </IonSegment>
+    );
+  };
+
+  const renderContent = () => {
+    if (tab === "photoGrid") {
+      return <PhotoGrid />;
+    }
+
+    if (tab === "instagram") {
+      return <InstagramWidget />;
+    }
+  };
+
   return (
     <div>
       {/* <Landing /> */}
-      <PhotoGrid />
+      {renderTabs()}
+      {renderContent()}
       {/* <MobileModal /> */}
-      <InstagramWidget />
     </div>
   );
 };
